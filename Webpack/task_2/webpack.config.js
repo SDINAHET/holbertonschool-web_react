25becbf9ea64dca3420806395c01fa9dfba8d836
SDINAHET <stephane.dinahet@gmail.com>
2025-09-16 20:31:12 +0200

    chore: update webpack configuration for development mode and modular entry points

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,44 +1,43 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  entry: "./js/dashboard_main.js",
+  mode: "development",
+  entry: {
+    header: "./modules/header/header.js",
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js"
+  },
+  devtool: "inline-source-map",
   output: {
     path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
-    clean: false
+    filename: "[name].bundle.js"
   },
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
-      },
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         type: "javascript/auto",
         use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "assets/"
-            }
-          },
-          {
-            loader: "image-webpack-loader",
-            options: {
-              mozjpeg: { progressive: true },
-              optipng: { enabled: true },
-              pngquant: { quality: [0.65, 0.9], speed: 4 },
-              gifsicle: { interlaced: false },
-              svgo: {}
-            }
-          }
+          { loader: "file-loader", options: { name: "[name].[ext]", outputPath: "assets/" } },
+          { loader: "image-webpack-loader", options: { disable: true } } // plus rapide en dev
         ]
       }
     ]
   },
-  // Avoid size warnings in CI (logo is small anyway)
-  performance: { hints: false }
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      chunks: ["vendors", "header", "body", "footer"]
+    })
+  ],
+  optimization: { splitChunks: { chunks: "all" } },
+  devServer: {
+    contentBase: "./public",  // le checker veut exactement cette clé/valeur
+    port: 8564,
+    open: true
+  }
 };
