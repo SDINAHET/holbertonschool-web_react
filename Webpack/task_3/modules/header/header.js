0fe143be9ac2db271c9a2f6e599b7ce28eaafb6a
SDINAHET <stephane.dinahet@gmail.com>
2025-09-18 12:56:03 +0200

    style: remove unused CSS rules and clean up header.js for consistency

--- Webpack/task_3/modules/header/header.js
+++ Webpack/task_3/modules/header/header.js
@@ -1,10 +1,11 @@
-import $ from 'jquery';
-import './header.css';
+import $ from "jquery";
+import "./header.css";
 
-console.log('Init header'); // the checker looks for this exact log
+console.log('Init header');
 
-// simple logo (text placeholder or actual <img/> if you have one)
-const $logo = $('<span class="hb-logo" aria-hidden="true"></span>');
-const $title = $('<h1>Holberton Dashboard</h1>');
+const $header = $("<header></header>");
+const $logo = $('<div id="logo"></div>');
+const $title = $("<h1>Holberton Dashboard</h1>");
 
-$('body').prepend($('<header id="app-header"></header>').append($logo, $title));
+$header.append($logo, $title);
+$("body").prepend($header);
