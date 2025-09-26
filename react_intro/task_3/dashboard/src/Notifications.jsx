d7e0a05b06b4f7bfd14ddf84b6caa4c1382b7df5
SDINAHET <stephane.dinahet@gmail.com>
2025-09-26 15:37:38 +0200

    refactor: simplify close button click handling and update notification list item attributes

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -4,16 +4,12 @@ import closeIcon from './assets/close-icon.png';
 import { getLatestNotification } from './utils';
 
 export default function Notifications() {
-  const handleClick = () => {
-    console.log('Close button has been clicked');
-  };
-
   return (
-    <div className="notifications">
+    <div className="notifications" style={{ position: 'relative' }}>
       {/* Bouton Close */}
       <button
         aria-label="Close"
-        onClick={handleClick}
+        onClick={() => console.log('Close button has been clicked')}
         style={{
           position: 'absolute',
           top: '10px',
@@ -26,14 +22,17 @@ export default function Notifications() {
         <img src={closeIcon} alt="close" style={{ width: '10px', height: '10px' }} />
       </button>
 
-      {/* Message */}
+      {/* Titre exact */}
       <p>Here is the list of notifications</p>
 
       {/* Liste des notifications */}
       <ul>
-        <li data-notification-type="default">New course available</li>
-        <li data-notification-type="urgent">New resume available</li>
-        <li data-notification-type="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
       </ul>
     </div>
   );
