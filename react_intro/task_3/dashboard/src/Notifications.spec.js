021e71b502c2cc70086cd768e6d4ee36ff7c9212
SDINAHET <stephane.dinahet@gmail.com>
2025-09-26 15:50:58 +0200

    refactor: correct case sensitivity for notifications title in tests

--- react_intro/task_3/dashboard/src/Notifications.spec.js
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -8,7 +8,7 @@ describe('Notifications component', () => {
   test('renders the notifications title (case-insensitive)', () => {
     render(<Notifications />);
     expect(
-      screen.getByText(/here is the list of notifications/i)
+      screen.getByText(/Here is the list of notifications/i)
     ).toBeInTheDocument();
   });
 
