9ddcc758747f1a629ba292799be3e5ac3a0e9688
SDINAHET <stephane.dinahet@gmail.com>
2025-10-05 02:26:53 +0200

    Refactor code structure for improved readability and maintainability

--- react_props/task_4/dashboard/src/CourseList/CourseList.jsx
+++ react_props/task_4/dashboard/src/CourseList/CourseList.jsx
@@ -1,36 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import CourseListRow from './CourseListRow.jsx';
-import './CourseList.css';
+import React from "react";
+import CourseListRow from "./CourseListRow";
+import './CourseList.css'
 
-export default function CourseList({ courses = [] }) {
-  const hasCourses = Array.isArray(courses) && courses.length > 0;
-
-  return (
-    <table id="CourseList" className="course-list">
-      <thead>
-        <CourseListRow isHeader textFirstCell="Available courses" textSecondCell={null} />
-        <CourseListRow isHeader textFirstCell="Course name" textSecondCell="Credit" />
-      </thead>
-      <tbody>
-        {hasCourses ? (
-          courses.map((c) => (
-            <CourseListRow key={c.id} textFirstCell={c.name} textSecondCell={c.credit} />
-          ))
-        ) : (
-          <CourseListRow textFirstCell="No course available yet" textSecondCell={null} />
-        )}
-      </tbody>
-    </table>
-  );
+export default function CourseList({ courses=[] }) {
+	return (
+		<table id="CourseList">
+			{courses.length > 0 ? (
+				<>
+					<thead>
+						<CourseListRow isHeader={true} textFirstCell="Available courses" />
+						<CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
+					</thead>
+					<tbody>
+						{courses.map((course) => (
+							<CourseListRow
+								key={course.id}
+								textFirstCell={course.name}
+								textSecondCell={course.credit}
+							/>
+						))}
+					</tbody>
+				</>
+				) : (
+					<tbody>
+						<CourseListRow isHeader={true} textFirstCell="No course available yet" />
+					</tbody>
+					)}
+		</table>
+	);
 }
-
-CourseList.propTypes = {
-  courses: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      credit: PropTypes.number.isRequired,
-    })
-  ),
-};
--- react_props/task_4/dashboard/src/CourseList/CourseListRow.jsx
+++ react_props/task_4/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,45 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
 
-export default function CourseListRow({
-  isHeader = false,
-  textFirstCell = '',
-  textSecondCell = null,
-}) {
-  if (isHeader) {
-    if (textSecondCell === null) {
-      return (
-        <tr>
-          <th colSpan="2">{textFirstCell}</th>
-        </tr>
-      );
-    }
-    return (
-      <tr>
-        <th>{textFirstCell}</th>
-        <th>{textSecondCell}</th>
-      </tr>
-    );
-  }
-
-  if (textSecondCell === null) {
-    return (
-      <tr>
-        <td colSpan="2">{textFirstCell}</td>
-      </tr>
-    );
-  }
-
-  return (
-    <tr>
-      <td>{textFirstCell}</td>
-      <td>{textSecondCell}</td>
-    </tr>
-  );
+export default function CourseListRow({ isHeader=false, textFirstCell="", textSecondCell=null }) {
+return (
+	<tr>
+		{isHeader ? (
+			textSecondCell === null ? (
+				<th colSpan="2">{textFirstCell}</th>
+			) : (
+				<>
+					<th style={{ width: '70%'}}>{textFirstCell}</th>
+					<th>{textSecondCell}</th>
+				</>
+			)
+		) : (
+			<>
+				<td>{textFirstCell}</td>
+				<td>{textSecondCell}</td>
+			</>
+		)}
+	</tr>
+);
 }
-
-CourseListRow.propTypes = {
-  isHeader: PropTypes.bool,
-  textFirstCell: PropTypes.string,
-  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-};
