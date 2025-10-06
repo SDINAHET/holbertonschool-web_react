43e81476eef75dc1c885723199cff18e1207b530
SDINAHET <stephane.dinahet@gmail.com>
2025-10-06 23:45:10 +0200

    Fix course list initialization and update notifications list structure

--- react_props/task_5/dashboard/src/App/App.jsx
+++ react_props/task_5/dashboard/src/App/App.jsx
@@ -7,18 +7,18 @@ import Notifications from '../Notifications/Notifications.jsx';
 import CourseList from '../CourseList/CourseList';
 import './App.css';
 
-const coursesList = [];
+// const coursesList = [];
 
-// const coursesList = [
-//   { id: 1, name: 'ES6', credit: 60 },
-//   { id: 2, name: 'Webpack', credit: 20 },
-//   { id: 3, name: 'React', credit: 40 },
-// ];
+const coursesList = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
 
-// const defaultNotifications = [
-//   { id: 1, type: 'default', value: 'New course available' },
-//   { id: 2, type: 'urgent', value: 'New resume available' },
-// ];
+const notificationsList = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+];
 
 // function App({ isLoggedIn = true, courses = coursesList }) {
 function App({ isLoggedIn = false, courses = coursesList }) {
