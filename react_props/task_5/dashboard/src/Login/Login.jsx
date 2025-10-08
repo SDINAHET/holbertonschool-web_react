28ba53e57972a1dd8ef05dc044668859ad122a6c
SDINAHET <stephane.dinahet@gmail.com>
2025-10-08 17:57:16 +0200

    Refactor code structure for improved readability and maintainability

--- react_props/task_5/dashboard/src/Login/Login.jsx
+++ react_props/task_5/dashboard/src/Login/Login.jsx
@@ -5,11 +5,11 @@ export default function Login() {
   return (
     <div className="App-body">
       <p>Login to access the full dashboard</p>
-      <label htmlFor="email">Email:</label>
+      <label htmlFor="email">Email</label>
       <input id="email" type="email" />
-      <label htmlFor="password">Password:</label>
+      <label htmlFor="password">Password</label>
       <input id="password" type="password" />
-      <button>OK</button>
+      <button>ok</button>
     </div>
   );
 }
