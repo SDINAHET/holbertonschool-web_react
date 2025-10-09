21569b6302893eeab5f1af0467c4666de68bd8a2
SDINAHET <stephane.dinahet@gmail.com>
2025-10-09 17:54:09 +0200

    Refactor BodySection styles: remove unused CSS and update imports for consistency

--- react_component/task_3/dashboard/src/BodySection/BodySection.jsx
+++ react_component/task_3/dashboard/src/BodySection/BodySection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './BodySection.css';
+import './BodySectionWithMarginBottom.css';
 
 const BodySection = ({ title, children }) => {
   return (
--- react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
+++ react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BodySection from './BodySection';
-import './BodySection.css';
+import './BodySectionWithMarginBottom.css';
 
 const BodySectionWithMarginBottom = ({ title, children }) => {
   return (
