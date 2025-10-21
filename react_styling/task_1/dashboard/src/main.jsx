c609e8b4a003ac0366adfd548d1bf51c708720e7
SDINAHET <stephane.dinahet@gmail.com>
2025-10-21 16:32:28 +0200

    feat: add main App component with notifications, header, footer, and conditional rendering for login and course list

--- react_styling/task_1/dashboard/src/main.jsx
+++ react_styling/task_1/dashboard/src/main.jsx
@@ -4,7 +4,8 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './main.css';
-import App from './App/App.jsx';
+// import App from './App/App.jsx';
+import App from './App.jsx'; // ⬅️ IMPORTANT
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
