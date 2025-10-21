c3c5cc0a6ec7c691e74e864918343032537842e3
SDINAHET <stephane.dinahet@gmail.com>
2025-10-21 12:10:51 +0200

    Refactor code structure for improved readability and maintainability

--- react_styling/task_0/dashboard/vite.config.js
+++ react_styling/task_0/dashboard/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // plugins: [react()],
-  plugins: [react(), tailwindcss()],
-  // base: '/holbertonschool-web_react/',
+  plugins: [react()],
+  base: '/holbertonschool-web_react/',
 })
