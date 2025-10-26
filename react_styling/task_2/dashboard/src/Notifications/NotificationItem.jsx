5bdfd8a3d31b953242dd5a3cdcec5bfe958982ad
SDINAHET <stephane.dinahet@gmail.com>
2025-10-27 00:10:46 +0100

    Refactor code structure for improved readability and maintainability

--- react_styling/task_2/dashboard/src/Notifications/NotificationItem.jsx
+++ react_styling/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,78 +1,82 @@
-// import React from 'react';
-// import React, { Component } from 'react';
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import closebtn from "../assets/close-button.png";
+import NotificationItem from "./NotificationItem";
 
-// export default function NotificationItem({ type = 'default', html, value }) {
-//   const style = { color: type === 'urgent' ? 'red' : 'blue' };
-
-//   if (html) {
-//     return (
-//       <li
-//         data-notification-type={type}
-//         style={style}
-//         // html = { __html: '...' }
-//         dangerouslySetInnerHTML={html}
-//       />
-//     );
-//   }
-
-//   return (
-//     <li data-notification-type={type} style={style}>
-//       {value}
-//     </li>
-//   );
-// }
-
-// NotificationItem.propTypes = {
-//   type: PropTypes.string,
-//   value: PropTypes.string,
-//   html: PropTypes.shape({ __html: PropTypes.string }),
-// };
-
-export default class NotificationItem extends PureComponent {
-// export default class NotificationItem extends Component {
+export default class Notifications extends Component {
   static propTypes = {
-    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-    type: PropTypes.string,
-    value: PropTypes.string,
-    html: PropTypes.shape({ __html: PropTypes.string }),
-    markAsRead: PropTypes.func, // passée depuis Notifications
+    notifications: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+        type: PropTypes.string,
+        value: PropTypes.string,
+        html: PropTypes.shape({ __html: PropTypes.string }),
+      })
+    ),
+    displayDrawer: PropTypes.bool,
   };
 
   static defaultProps = {
-    type: 'default',
-    markAsRead: () => {},
+    notifications: [],
+    displayDrawer: false,
   };
 
-  handleClick = () => {
-    const { id, markAsRead } = this.props;
-    markAsRead(id);
+  shouldComponentUpdate(nextProps) {
+    return nextProps.notifications.length !== this.props.notifications.length;
+  }
+
+  markAsRead = (id) => {
+    console.log(`Notification ${id} has been marked as read`);
   };
 
   render() {
-    const { type, html, value } = this.props;
-    const style = { color: type === 'urgent' ? 'red' : 'blue' };
+    const { notifications, displayDrawer } = this.props;
 
-    if (html) {
-      return (
-        <li
-          data-notification-type={type}
-          style={style}
-          onClick={this.handleClick}
-          dangerouslySetInnerHTML={html}
-        />
-      );
-    }
+    const Title = (
+      <div className="text-right pr-4 font-bold text-lg text-[var(--main-color)]">
+        Your notifications
+      </div>
+    );
+
+    const Drawer = displayDrawer ? (
+      <div
+        className="border-2 border-dashed p-4 mt-2"
+        style={{ borderColor: "var(--main-color)" }}
+      >
+        {notifications.length > 0 ? (
+          <>
+            <p>Here is the list of notifications</p>
+            <button
+              onClick={() => console.log("Close button has been clicked")}
+              aria-label="Close"
+              className="absolute top-2 right-2"
+            >
+              <img src={closebtn} alt="Close" />
+            </button>
+            <ul>
+              {notifications.map((n) => (
+                <NotificationItem
+                  key={n.id}
+                  id={n.id}
+                  type={n.type}
+                  value={n.value}
+                  html={n.html}
+                  markAsRead={this.markAsRead}
+                />
+              ))}
+            </ul>
+          </>
+        ) : (
+          <p>No new notification for now</p>
+        )}
+      </div>
+    ) : null;
 
     return (
-      <li
-        data-notification-type={type}
-        style={style}
-        onClick={this.handleClick}
-      >
-        {value}
-      </li>
+      <>
+        {Title}
+        {Drawer}
+      </>
     );
   }
 }
