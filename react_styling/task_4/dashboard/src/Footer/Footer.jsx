d6bfa44ec43502828656bd7ef74f151e60846151
SDINAHET <stephane.dinahet@gmail.com>
2025-10-28 15:16:03 +0100

    refactor: update footer copyright text for clarity

--- react_styling/task_4/dashboard/src/Footer/Footer.jsx
+++ react_styling/task_4/dashboard/src/Footer/Footer.jsx
@@ -4,7 +4,8 @@ export default function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="App-footer border-t-[3px] border-[var(--main-color)] py-2 text-center italic">
-      <p>Copyright {year} - Holberton School</p>
+      {/* <p>Copyright {year} - Holberton School</p> */}
+      <p>Copyright {year} - Holberton School main dashboard</p>
     </footer>
   );
 }
