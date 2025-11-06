305c2ba7d3b3ac6bea3683befbf8627514e87f0d
SDINAHET <stephane.dinahet@gmail.com>
2025-11-06 01:58:22 +0100

    Refactor code structure for improved readability and maintainability

--- react_hooks/task_0/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_0/dashboard/src/Footer/Footer.jsx
@@ -1,17 +1,19 @@
 // import React from 'react';
 import React, { useContext } from 'react';
-import AppContext from '../Context/context';
+import AppContext, { defaultUser } from '../Context/context';
 
 export default function Footer() {
-  const context = useContext(AppContext) || {};
+  const ctx = useContext(AppContext) || {};
   // const { user } = useContext(AppContext);
-  const { user } = context;
+  // const { user } = context;
+  // const year = new Date().getFullYear();
+  const user = ctx.user ?? defaultUser;
   const year = new Date().getFullYear();
 
   return (
     <footer className="App-footer border-t-[3px] border-[var(--main-color)] py-2 text-center italic">
       <p>Copyright {year} - Holberton School</p>
-      {user?.isLoggedIn && (
+      {user.isLoggedIn && (
         <p>
           <a href="#">Contact us</a>
         </p>
