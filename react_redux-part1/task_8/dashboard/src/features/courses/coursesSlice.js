7e4b50dccab14c1eacbb458cb70a200533bb7bae
SDINAHET <stephane.dinahet@gmail.com>
2025-11-19 06:38:25 +0100

    Refactor code structure for improved readability and maintainability

--- react_redux-part1/task_8/dashboard/src/features/courses/coursesSlice.js
+++ react_redux-part1/task_8/dashboard/src/features/courses/coursesSlice.js
@@ -1,11 +1,53 @@
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import { logout } from '../auth/authSlice';
+
+// // const API_BASE_URL = 'http://localhost:5173';
+
+// // const ENDPOINTS = {
+// //   courses: `${API_BASE_URL}/courses.json`,
+// // };
+
+// const initialState = {
+//   courses: [],
+// };
+
+// export const fetchCourses = createAsyncThunk(
+//   'courses/fetchCourses',
+//   async () => {
+//     // const response = await fetch(ENDPOINTS.courses);
+//     const response = await fetch("/courses.json");
+//     const data = await response.json();
+//     return data;
+//   }
+// );
+
+// const coursesSlice = createSlice({
+//   name: 'courses',
+//   initialState,
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchCourses.fulfilled, (state, action) => {
+//         state.courses = action.payload;
+//       })
+//       .addCase(logout, (state) => {
+//         state.courses = [];
+//       });
+//   },
+// });
+
+// export default coursesSlice.reducer;
+
+// src/features/courses/coursesSlice.js
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 import { logout } from '../auth/authSlice';
 
-// const API_BASE_URL = 'http://localhost:5173';
+const API_BASE_URL = 'http://localhost:5173';
 
-// const ENDPOINTS = {
-//   courses: `${API_BASE_URL}/courses.json`,
-// };
+const ENDPOINTS = {
+  courses: `${API_BASE_URL}/courses.json`,
+};
 
 const initialState = {
   courses: [],
@@ -14,10 +56,8 @@ const initialState = {
 export const fetchCourses = createAsyncThunk(
   'courses/fetchCourses',
   async () => {
-    // const response = await fetch(ENDPOINTS.courses);
-    const response = await fetch("/courses.json");
-    const data = await response.json();
-    return data;
+    const response = await axios.get(ENDPOINTS.courses);
+    return response.data;
   }
 );
 
@@ -28,8 +68,9 @@ const coursesSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchCourses.fulfilled, (state, action) => {
-        state.courses = action.payload;
+        state.courses = action.payload || [];
       })
+      // Quand on se déconnecte, on vide la liste des cours
       .addCase(logout, (state) => {
         state.courses = [];
       });
@@ -37,3 +78,4 @@ const coursesSlice = createSlice({
 });
 
 export default coursesSlice.reducer;
+
